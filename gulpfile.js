e1217a8971e79e05b452cdd235c9acd87a227baa
bab-gam <81931897+bab-gam@users.noreply.github.com>
2022-01-20 21:41:15 +0300

    (*/ω＼*) (╯°□°）╯︵ ┻━┻

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,11 @@ import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import sass from 'gulp-dart-sass';
 import postcss from 'gulp-postcss';
+import csso from 'postcss-csso';
 import autoprefixer from 'autoprefixer';
+import rename from 'gulp-rename';
 import browser from 'browser-sync';
+import htmlmin from 'gulp-htmlmin';
 
 // Styles
 
@@ -12,10 +15,22 @@ export const styles = () => {
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
+    .pipe(rename('style.min.css'))
     .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
+}
+
+// html
+// не рабоатет аааааааааааааааааааааааа, у меня скоро так нервов не будет. ладно я продлжу, а с этим потом
+
+export const html = () => {
+  return gulp.src('source/*.html')
+  .pipe(plumber())
+  .pipe(htmlmin({ collapseWhitespace: true }))
+  .pipe(gulp.dest('bulid'));
 }
 
 // Server
